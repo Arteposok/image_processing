92a3de556ff0f7bc525378d8c43596be92b26938
Arteposok <aeposokhov@gmail.com>
2025-02-23 21:47:42 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,98 @@
+import cv2 as cv
+import mss
+import numpy as np
+import time
+import tkinter as tk
+import random as rnd
+
+def filter(img):
+    edit = img
+    edit = cv.bilateralFilter(edit, d=2, sigmaColor=20, sigmaSpace=30)
+    edges_colored = cv.Canny(edit, threshold1=50, threshold2=150)
+    edges_colored = cv.cvtColor(edges_colored, cv.COLOR_GRAY2BGR)
+    edges_colored=cv.GaussianBlur(edges_colored, (5,5), 0)
+    edges_colored=cv.bitwise_not(edges_colored)
+    edit = cv.bilateralFilter(edit, d=5, sigmaColor=50, sigmaSpace=50)
+    edit = cv.addWeighted(edit, 0.2, edges_colored, 0.8,0)
+    return edit
+
+def grab(sct):
+    return np.array(sct.grab(sct.monitors[1]))[:, :, :3]
+
+def video_loop():
+    open_it=True
+    fast_save=False
+    stich=True
+    capture=True
+    file_in = "horse_sample.mp4"
+    file_out = "camera.mp4"
+    video = cv.VideoCapture(0)#file_in if open_it else 0)
+    if not video.isOpened():
+        print("Error: Couldn't open video.")
+        exit()
+    codec = cv.VideoWriter_fourcc(*'mp4v')
+    screen=np.array(mss.mss().grab(mss.mss().monitors[1]))[:,:,:3]
+    fps = video.get(cv.CAP_PROP_FPS) if open_it else 100
+    frame_time = 1 / fps
+    width = int(video.get(cv.CAP_PROP_FRAME_WIDTH)) if open_it else len(screen[0])
+    width *= 2 if stich else 1
+    height = int(video.get(cv.CAP_PROP_FRAME_HEIGHT)) if open_it else len(screen)
+    out = cv.VideoWriter(file_out, codec, fps, (width, height))
+    frame_n=0
+    shoot_next_frame=False
+    win=tk.Tk()
+    win.resizable=False
+    def shoot(*x):
+        nonlocal shoot_next_frame
+        shoot_next_frame=True
+    shot_button=tk.Button(win,text="snapshot", command=shoot)
+    shot_button.pack()
+    with mss.mss() as sct:
+        while video.isOpened():
+            win.update()
+            prev_time = time.perf_counter()
+            if open_it:
+                ret, frame = video.read()
+                if not ret:
+                    break
+            else:
+                frame = grab(sct)
+            if open_it:
+                frame_show=cv.resize(frame, (width*2,height*2))
+            edit = filter(frame)
+            edit = cv.hconcat([frame,edit])
+            out.write(edit)
+            if capture and shoot_next_frame:
+                x=str(rnd.randint(0,10))
+                y=str(rnd.randint(0,10))
+                z=str(rnd.randint(0,10))
+                cv.imwrite(x+y+z+".png", edit)
+                shoot_next_frame=False
+            if not fast_save:
+                edit_show=cv.resize(edit,(width*2,height*2))
+                cv.imshow("edit", edit_show)
+            if fast_save:
+                continue
+            delay = time.perf_counter() - prev_time
+            frame_n += 1
+            wait = max(0, frame_time - delay)
+            time.sleep(wait)
+            print('delay ' + str(delay))
+            print('wait time ' + str(wait))
+            print("processed! frame N_*" + str(frame_n))
+            if cv.waitKey(1) & 0xFF == ord("q") and not fast_save:
+                break
+
+
+    video.release()
+    out.release()
+
+def image_shot():
+    image=cv.imread("sample4.png")
+    image=filter(image)
+    image2=cv.resize(image, (int(len(image[0])/2), int(len(image)/2)))
+    cv.imshow("edit", image2)
+    #cv.imwrite("sample1_out.webp", image)
+    cv.waitKey()
+
+video_loop()
