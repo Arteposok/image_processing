6eb698097779a0cc141ff037135223690765806e
Arteposok <aeposokhov@gmail.com>
2025-02-24 15:23:16 +0400

    'm'

--- git.py
+++ git.py
@@ -1,5 +1,12 @@
 import os
+import keyboard
 
-os.system("git add .")
-os.system("git commit -m 'm'")
-os.system("git push -u origin main")
+def save():
+    os.system("git add .")
+    os.system("git commit -m 'm'")
+    os.system("git push -u origin main")
+
+keyboard.add_hotkey("ctrl+shift+space", save)
+
+while True:
+    pass
